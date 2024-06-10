fc7a5f1d24ac34ef87b023414011545578eee4b3
Monika <154052331+monikaffs@users.noreply.github.com>
2024-06-10 21:44:18 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>smoodh</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+    <header>
+        
+<div class="data-container">
+   
+       <img src="logo2.png" class="logo"></a></div>
+        <ul>
+            <a class="hover2" href="home.php" target="_blank">Home</a>
+            <a class="hover2" href="review.php" target="_blank">Reviews</a>
+            <a class="hover2" href="order1.php" target="_blank">Menu</a>
+            <a class="hover2" href="login_form.php" target="_blank">Log in</a>
+            <a class="hover2" href="register_form.php" target="_blank">Register</a>
+        </ul>
+    </div>
+    </header>
+    <div class="container2">
+        <div class="main2">
+            <img src="smoodh8.png" alt="smoodh" class="main-img">
+        </div>  
+</div>
+    </div>
+            </div>
+            <div class="container">
+                <div class="text-box">
+                    <div class="shadow-box">
+                        <h2>Its not just a drink <br>Its  <span>Smoodh</span> <br>Your Daily Boost!!<br> 
+                        </h2>
+                    </div>
+                    <br>
+                    <br>
+                    <div class="relative text" >
+                        <p>Indulge in the silky, creamy and rich taste of <strong> Parle Agro's </strong> 
+                            dairy based flavoured drink, <b>SMOODH</b>. Available in 4 yummy flavors; 
+                            <i>Chocolate, Toffee Caramel, Coffee Frappé & Hazelnut Chocolate</i>. SMOODH 
+                            satisfies your craving for a quick energy boosting, sweet fix that is 
+                            absolutely divine. Crafted combining the finest quality ingredients and 
+                            goodness of milk, <b>SMOODH</b> is a deliciously wholesome and 
+                            nourishing offering that will not just tingle your taste buds, but liven  
+                            up your day in a healthy way.</p>
+                            <br>
+                        <a href="3">Learn More</a>
+                   </div>
+
+                   <ul class="thumb ">
+                    <div class="circle"></div>
+                    <li><img src="smoodh11.png" onclick="imgSlidr('smoodh11.png');changeCircleColor('#017143')"></li>
+                    <li><img src="smoodh12.png" onclick="imgSlider('smoodh12.png');changeCircleColor('#017143')"></li>
+                    <li><img src="smoodh8.png" onclick="imgSlider('smoodh8.png');changeCircleColor('#0eb7495')"></li>
+                    <li><img src="smoodh9.png" onclick="imgSlider('smoodh9.png');changeCircleColor('#d752b1')"></li>
+                </ul>
+                <br>
+                </div>
+            <br>
+
+        </div>
+    </div>
+
+    <footer>
+    <div class="container2">
+        <p1> &copy; 2024 Parle Agro SMOODH. All rights reserved.</p1>
+    </div>
+</footer>
+</body>
+</html>
